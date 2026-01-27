b2bd41f90cbc1d97eb89eb2ffdc2f96930cec914
Brayan1902 <marchena1902@gmail.com>
2026-01-27 11:53:59 -0500

    Iconos de desplazamiento
    Se cambiaron los iconos de desplazamiento de la pagina de inicio, y adicional se cambio el color del nombre del producto en la vista producto

--- src/components/Apartado.js
+++ src/components/Apartado.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Apartado.css";
+import arrowLeft from "../Assets/circulo-de-flecha2.png"; 
+import arrowRigth from "../Assets/circulo-de-flecha1.png"; 
 
 import img1 from "../Assets/accesorios.png";
 import img2 from "../Assets/accesorios.png";
@@ -40,10 +42,11 @@ function Apartado() {
       </div>
 
       <button className="apartado-arrow left" onClick={prev}>
-        ‹
+        <img src={arrowLeft} alt="Anterior"/>
       </button>
+
       <button className="apartado-arrow right" onClick={next}>
-        ›
+        <img src={arrowRigth} alt="Siguiente"/>
       </button>
     </section>
   );
